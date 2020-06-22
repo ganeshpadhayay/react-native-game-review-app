6aae78cdcd229a3b0da7504ba0d3ae6f77b6ae66
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-06-22 19:35:29 +0530

    stack navigation added

---
+++ routes/homeStack.js
@@ -0,0 +1,33 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Home from "../screens/home";
+import ReviewDetails from "../screens/reviewDetails";
+
+const screens = {
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "GameZone",
+      // headerStyle: { backgroundColor: "#eee" },
+    },
+  },
+  ReviewDetails: {
+    screen: ReviewDetails,
+    navigationOptions: {
+      title: "Review Details",
+      // headerStyle: { backgroundColor: "#eee" },
+    },
+  },
+};
+
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: "#808080",
+      height: 70,
+      headerTintColor: "#444",
+    },
+  },
+});
+
+export default createAppContainer(HomeStack);
--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,36 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, FlatList, TouchableOpacity, Text } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const [reviews, setReviews] = useState([
+    {
+      title: "Zelda, Breath of Fresh Air",
+      rating: 5,
+      body: "lorem ipsum",
+      key: "1",
+    },
+    { title: "Gotta catch them all", rating: 4, body: "lorem ipsum", key: "2" },
+    { title: "Final Fantasy", rating: 3, body: "lorem ipsum", key: "3" },
+  ]);
+
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+    // navigation.push("ReviewDetails")
+  };
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen!</Text>
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
