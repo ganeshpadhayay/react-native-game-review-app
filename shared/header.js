15b318c9fd3a958a4276f9c22ba76e8d645fd8c9
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-06-22 22:03:28 +0530

    added card and images

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 //we have navigation object in prop so simply destructure it
@@ -9,17 +9,24 @@ export default function Header({ navigation, title }) {
   };
 
   return (
-    <View style={styles.header}>
+    <ImageBackground
+      source={require("../assets/game_bg.png")}
+      style={styles.header}
+    >
       <MaterialIcons
         name="menu"
         size={28}
         onPress={openMenuHandler}
         style={styles.icon}
       />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image
+          source={require("../assets/heart_logo.png")}
+          style={styles.headerImage}
+        />
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -40,5 +47,13 @@ const styles = StyleSheet.create({
   icon: {
     position: "absolute",
     left: 0,
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10,
+  },
+  headerTitle: {
+    flexDirection: "row",
   },
 });
